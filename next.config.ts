d8bee2a593c24c7dcef68f9983ca2e716fd7f485
awynx <alwin.puche16@gmail.com>
2025-02-25 18:50:28 +0800

    Add image configuration and redirects in next.config; update package.json with new dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,35 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
+	images: {
+		remotePatterns: [
+			{
+				hostname: "nkmkpzzqjrewuynxaftt.supabase.co",
+				pathname: "/storage/**",
+				protocol: "https",
+				port: "",
+				search: "",
+			},
+		],
+		deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+		imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+	},
+	redirects: async () => {
+		return [
+			{
+				source: "/events/:slug",
+				destination: "/events/:slug/info",
+				has: [
+					{
+						type: "query",
+						key: "slug",
+					},
+				],
+				permanent: false, // Set to true for a permanent redirect (301), false for temporary (302)
+			},
+		];
+	},
 };
 
 export default nextConfig;
