6b8e0923e5ab7c31cf7277e29a092681729f4460
awynx <alwin.puche16@gmail.com>
2025-04-18 22:23:26 +0800

    feat: implement createAnnouncement function to handle announcement creation with unique slug generation

---
+++ src/models/announcement.ts
@@ -0,0 +1,23 @@
+import prisma from "@/lib/prisma";
+import { Announcement } from "@prisma/client";
+import slug from "unique-slug";
+
+export const createAnnouncement = async ({
+	content,
+	coverImg,
+	title,
+}: Pick<Announcement, "title" | "content" | "coverImg">) => {
+	const timestamp = Date.now(); // current timestamp
+	const randomPart = Math.random().toString(36).substring(2, 10); // random string (base 36)
+	const name = title.toLowerCase().replace(/ /g, "-");
+	const generatedSlug = slug(name);
+
+	return await prisma.announcement.create({
+		data: {
+			title,
+			content,
+			coverImg,
+			slug: `${name}-${timestamp}-${randomPart}-${generatedSlug}`,
+		},
+	});
+};
