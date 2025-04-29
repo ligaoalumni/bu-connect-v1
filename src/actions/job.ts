e84f6b79b8701a86588de9bfe4a4f10237fbda5a
awynx <alwin.puche16@gmail.com>
2025-04-29 21:44:50 +0800

    feat: add job reading functionality and enhance job update logic with authorization checks

--- src/actions/job.ts
+++ src/actions/job.ts
@@ -1,8 +1,9 @@
 "use server";
 
 import { decrypt } from "@/lib/session";
-import { createJob, updateJob } from "@/repositories";
-import { JobData, UpdateJob } from "@/types";
+import { createJob, readJob, readJobs, updateJob } from "@/repositories";
+import { JobData, PaginationArgs, UpdateJob } from "@/types";
+import { JobStatus } from "@prisma/client";
 import { cookies } from "next/headers";
 
 export const createJobAction = async (data: Omit<JobData, "userId">) => {
@@ -30,6 +31,38 @@ export const updateJobAction = async (toUpdateId: number, data: UpdateJob) => {
 		await updateJob(toUpdateId, Number(session?.id), data);
 	} catch (error) {
 		console.log(error);
+		throw new Error("Failed to update job");
+	}
+};
+
+export const readJobsAction = async (
+	data: PaginationArgs<JobStatus, never>
+) => {
+	try {
+		const cookieStore = await cookies();
+
+		const session = await decrypt(cookieStore.get("session")?.value);
+
+		return await readJobs({
+			...data,
+			userId: Number(session?.id),
+			isAdmin: session?.role !== "ALUMNI",
+		});
+	} catch (error) {
+		console.log(error);
 		throw new Error("Failed to create event");
+	}
+};
+
+export const readJobAction = async (id: number) => {
+	try {
+		const job = await readJob(id);
+
+		if (!job) return null;
+
+		return job;
+	} catch (error) {
+		console.log(error);
+		throw new Error("Failed to fetch job");
 	}
 };
--- src/repositories/job.ts
+++ src/repositories/job.ts
@@ -1,7 +1,8 @@
 "use server";
 
 import prisma from "@/lib/prisma";
-import { JobData, UpdateJob } from "@/types";
+import { JobData, PaginationArgs, PaginationResult, UpdateJob } from "@/types";
+import { Job, JobStatus, Prisma } from "@prisma/client";
 
 export const createJob = async ({
 	company,
@@ -34,11 +35,93 @@ export const updateJob = async (
 	postedById: number,
 	data: UpdateJob
 ) => {
+	const user = await prisma.user.findUnique({ where: { id: postedById } });
+
+	if (!user) {
+		throw new Error("Unauthorized");
+	}
+
+	if (user.id !== postedById && user.role === "ALUMNI") {
+		throw new Error("Unauthorized");
+	}
+
 	return await prisma.job.update({
 		where: {
 			id: toUpdate,
-			postedById,
 		},
 		data,
+	});
+};
+
+type TJobPagination = PaginationArgs<JobStatus, never> & {
+	isAdmin?: boolean;
+	userId: number;
+};
+
+export const readJobs = async ({
+	filter,
+	pagination,
+	order,
+	orderBy,
+	status,
+	userId,
+	isAdmin,
+}: TJobPagination): Promise<PaginationResult<Job>> => {
+	let where: Prisma.JobWhereInput = {};
+
+	if (filter && typeof filter === "number") {
+		where = {
+			id: filter,
+		};
+	}
+
+	if (status) {
+		where = {
+			...where,
+			status: {
+				in: status,
+			},
+		};
+	}
+
+	if (typeof filter === "string") {
+		where = {
+			OR: [
+				{ title: { contains: filter, mode: "insensitive" } },
+				{ jobTitle: { contains: filter, mode: "insensitive" } },
+			],
+		};
+	}
+
+	if (!isAdmin) {
+		where = {
+			...where,
+			postedBy: {
+				id: userId,
+			},
+		};
+	}
+
+	const jobs = await prisma.job.findMany({
+		where,
+		skip: pagination ? pagination.limit * pagination.page : undefined,
+		take: pagination ? pagination.limit : undefined,
+		orderBy: orderBy ? { [orderBy]: order || "asc" } : { id: "asc" },
+	});
+
+	const count = await prisma.job.count({ where });
+
+	return {
+		count,
+		hasMore: jobs.length === pagination?.limit,
+		data: jobs,
+	};
+};
+
+export const readJob = async (id: number) => {
+	return await prisma.job.findUnique({
+		where: {
+			id,
+		},
 	});
 };
