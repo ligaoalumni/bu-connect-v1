bffac166db87f0b7abd819dd70bf088b61661725
awynx <alwin.puche16@gmail.com>
2025-04-29 21:47:09 +0800

    feat: add job management pages including edit, add, and not found components

---
+++ src/app/admin/jobs/[slug]/edit/page.tsx
@@ -0,0 +1,39 @@
+import { readJobAction } from "@/actions";
+import { Button } from "@/components";
+import JobForm from "@/components/custom/job-form";
+import { Icon } from "@iconify/react/dist/iconify.js";
+import Link from "next/link";
+import React from "react";
+
+export default async function Page({
+	params,
+}: {
+	params: Promise<{ slug: string }>;
+}) {
+	const { slug } = await params;
+
+	if (isNaN(Number(slug))) return <div>Invalid Job ID</div>;
+
+	const job = await readJobAction(Number(slug));
+
+	if (!job) return <h1>Job Not Found</h1>;
+
+	return (
+		<div className="space-y-4">
+			<div className="flex gap-2 max-w-screen-lg mx-auto   ">
+				<Button className="h-10 w-10 p-0  " asChild>
+					<Link href="/admin/jobs" className="text-3xl">
+						<Icon icon="ci:chevron-left" width="24" height="24" className="" />
+					</Link>
+				</Button>
+				<div>
+					<h1 className="text-2xl leading-none font-bold">Edit Job</h1>
+					<p className="text-sm leading-none text-muted-foreground">
+						Edit the job details below.
+					</p>
+				</div>
+			</div>
+			<JobForm job={job} />
+		</div>
+	);
+}
