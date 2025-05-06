40c4fec9606430d7aa5d0037f655fdbf103186cf
awynx <alwin.puche16@gmail.com>
2025-05-06 08:12:27 +0800

    feat: refactor uploadImageAction to accept storage parameter for flexibility

--- src/actions/upload-image.ts
+++ src/actions/upload-image.ts
@@ -2,21 +2,22 @@
 import { createClient } from "@/lib/supabase";
 import { v4 as uuidv4 } from "uuid";
 
-export const uploadImageAction = async (file: File) => {
+export const uploadImageAction = async (
+	file: File,
+	storage = "event_covers"
+) => {
 	const db = await createClient();
 
 	try {
 		const name = `${file.name}-${uuidv4()}`;
-		const { data } = await db.storage.from("event_covers").upload(name, file, {
+		const { data } = await db.storage.from(storage).upload(name, file, {
 			cacheControl: "3600",
 			upsert: false,
 		});
 		if (data === null) {
 			return console.error("No data");
 		}
-		const response = await db.storage
-			.from("event_covers")
-			.getPublicUrl(data.path);
+		const response = await db.storage.from(storage).getPublicUrl(data.path);
 		if (response.data.publicUrl === null) {
 			return console.error("No data");
 		}
