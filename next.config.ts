f560a913280f912ba4818fc3bf34441c6caf629c
awynx <alwin.puche16@gmail.com>
2025-05-06 13:26:03 +0800

    feat: add redirect for posts to include slug information

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,17 @@ const nextConfig: NextConfig = {
 				permanent: false, // Set to true for a permanent redirect (301), false for temporary (302)
 			},
 			{
+				source: "/posts/:slug",
+				destination: "/posts/:slug/info",
+				has: [
+					{
+						type: "query",
+						key: "slug",
+					},
+				],
+				permanent: false, // Set to true for a permanent redirect (301), false for temporary (302)
+			},
+			{
 				source: "/admin/alumni/info",
 				destination: "/admin/alumni/records",
 				permanent: false, // Set to true for a permanent redirect (301), false for temporary (302)
