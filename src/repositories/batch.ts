ecb8845b40b20225738b452fcf09e5b9fc26d77a
awynx <alwin.puche16@gmail.com>
2025-05-07 15:46:28 +0800

    feat: add Batch interface with students property to types

--- src/repositories/batch.ts
+++ src/repositories/batch.ts
@@ -1,22 +1,39 @@
 import prisma from "@/lib/prisma";
-import { PaginationArgs } from "@/types";
+import { Batch, PaginationArgs, PaginationResult } from "@/types";
 
 export const readBatches = async ({
 	pagination,
 	orderBy,
 	order,
-}: PaginationArgs<never, never>) => {
+}: PaginationArgs<never, never>): Promise<PaginationResult<Batch>> => {
 	const batches = await prisma.batch.findMany({
 		skip: pagination ? pagination.limit * pagination.page : undefined,
 		take: pagination ? pagination.limit : undefined,
 		orderBy: orderBy ? { [orderBy]: order || "asc" } : { id: "asc" },
 	});
 
+	const batchesWithStudentCounts = await Promise.all(
+		batches.map(async (batch) => {
+			const students = await prisma.user.count({
+				where: {
+					batch: batch.batch,
+				},
+			});
+
+			return {
+				id: batch.id,
+				batch: batch.batch,
+				images: batch.images,
+				students,
+			};
+		})
+	);
+
 	const count = await prisma.batch.count();
 
 	return {
 		count,
 		hasMore: batches.length === pagination?.limit,
-		data: batches,
+		data: batchesWithStudentCounts,
 	};
 };
