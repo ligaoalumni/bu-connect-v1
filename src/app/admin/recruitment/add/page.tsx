76cbab1e6cdcc60fbed69c7fafbc253813387717
awynx <alwin.puche16@gmail.com>
2025-05-13 21:48:35 +0800

    feat: add recruitment page components for recruitment form and main page

---
+++ src/app/admin/recruitment/add/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { RecruitmentForm } from "../__components/recruitment-form";
+
+export default function Page() {
+	return (
+		<div>
+			<RecruitmentForm />
+		</div>
+	);
+}
