02005b2b95b3aeac9e7c078c0bf6719679a5396f
awynx <alwin.puche16@gmail.com>
2025-05-14 09:17:11 +0800

    feat: update recruitment page to include loading skeleton and data table

--- src/app/admin/recruitment/page.tsx
+++ src/app/admin/recruitment/page.tsx
@@ -1,5 +1,11 @@
-import React from "react";
+import React, { Suspense } from "react";
+import { TableSkeleton } from "../_components/table-skeleton";
+import RecruitmentDataTable from "./__components/recruitment-data-table";
 
 export default function Page() {
-	return <div>Page</div>;
+	return (
+		<Suspense fallback={<TableSkeleton />}>
+			<RecruitmentDataTable />
+		</Suspense>
+	);
 }
