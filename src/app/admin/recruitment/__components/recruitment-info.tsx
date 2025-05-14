de7d1c8d3b737b735bda45f711c7669572d19208
awynx <alwin.puche16@gmail.com>
2025-05-15 07:11:27 +0800

    feat: enhance RecruitmentInfo component with applicant count and creation date

--- src/app/admin/recruitment/__components/recruitment-info.tsx
+++ src/app/admin/recruitment/__components/recruitment-info.tsx
@@ -1,42 +1,82 @@
+"use client";
 import {
 	Badge,
 	Button,
 	Card,
 	CardContent,
 	CardDescription,
+	CardFooter,
 	CardHeader,
 	CardTitle,
+	Separator,
 } from "@/components";
 import { getIndustryName } from "@/lib/utils";
-import { Recruitment } from "@prisma/client";
-import { format } from "date-fns";
-import { Briefcase, CalendarIcon, Tag, Users } from "lucide-react";
+import { format, formatDistanceToNow } from "date-fns";
+import {
+	Briefcase,
+	CalendarIcon,
+	Tag,
+	UserRoundSearch,
+	Users,
+} from "lucide-react";
 import Link from "next/link";
+import ApplyDialog from "./apply";
+import { useAuth } from "@/contexts/auth-context";
+import { RecruitmentWithApplicants } from "@/types";
 
 export default function RecruitmentInfo({
 	recruitment,
 	batches,
+	isAdmin = false,
 }: {
-	recruitment: Recruitment;
+	recruitment: RecruitmentWithApplicants;
 	batches: number[];
+	isAdmin?: boolean;
 }) {
+	const { user } = useAuth();
+
+	const applicantsIds = recruitment.applicants.map((applicant) => applicant.id);
+
 	return (
 		<Card className="w-full">
 			<CardHeader>
+				{!isAdmin && (
+					<h3 className="text-gray-500 h-7  flex items-center gap-2">
+						<span>Posted by admin</span>
+						<Separator orientation="vertical" className="w-0.5" />
+						<span>
+							{formatDistanceToNow(recruitment.createdAt, {
+								addSuffix: true,
+							})}
+						</span>
+					</h3>
+				)}
 				<div className="flex justify-between items-start">
 					<div>
-						<CardTitle className="text-2xl">{recruitment.title}</CardTitle>
+						<CardTitle className="text-2xl">{recruitment.eventTitle}</CardTitle>
 						<CardDescription className="flex items-center mt-2">
 							<CalendarIcon className="mr-2 h-4 w-4" />
 							{format(new Date(recruitment.date), "PPP")}
 						</CardDescription>
 					</div>
-					<Button asChild variant="outline" size="sm">
-						<Link href={`/admin/recruitment/${recruitment.id}/edit`}>Edit</Link>
-					</Button>
+					{isAdmin && (
+						<Button asChild variant="outline" size="sm">
+							<Link href={`/admin/recruitment/${recruitment.id}/edit`}>
+								Edit
+							</Link>
+						</Button>
+					)}
 				</div>
 			</CardHeader>
 			<CardContent className="space-y-6">
+				<div>
+					<h3 className="text-sm font-medium flex items-center mb-2">
+						<UserRoundSearch className="mr-2 h-4 w-4" /> Recruiting
+					</h3>
+					<Badge variant="outline" className="text-sm">
+						{recruitment.recruiting}
+					</Badge>
+				</div>
 				<div>
 					<h3 className="text-sm font-medium flex items-center mb-2">
 						<Briefcase className="mr-2 h-4 w-4" /> Industry
@@ -74,8 +114,27 @@ export default function RecruitmentInfo({
 							</Badge>
 						))}
 					</div>
+					<div>
+						{recruitment.applicants.length > 0 ? (
+							<span className="text-sm text-gray-500">
+								{recruitment.applicants.length} applicants
+							</span>
+						) : (
+							<span className="text-sm text-gray-500">No applicants yet</span>
+						)}
+					</div>
 				</div>
 			</CardContent>
+			{!isAdmin && (
+				<CardFooter>
+					<ApplyDialog
+						alreadyApplied={applicantsIds.includes(Number(user?.id))}
+						eventTitle={recruitment.eventTitle}
+						recruitment={recruitment.recruiting}
+						recruitmentId={recruitment.id}
+					/>
+				</CardFooter>
+			)}
 		</Card>
 	);
 }
