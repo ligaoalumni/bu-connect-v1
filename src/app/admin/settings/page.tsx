9c7ff23fd4c735043e6aa0c3ccb0ff154bc54417
awynx <alwin.puche16@gmail.com>
2025-05-26 16:14:56 +0800

    add settings page

--- src/app/admin/settings/page.tsx
+++ src/app/admin/settings/page.tsx
@@ -1,5 +1,49 @@
-import React from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import GeneralSettings from "./__components/general";
+import LoginAttempts from "./__components/login-attemps";
+import { readLoginLogsAction, readSettingsAction } from "@/actions";
 
-export default function Page() {
-	return <div>Page</div>;
+export default async function AdminSettingsPage() {
+	const loginLogs = await readLoginLogsAction({
+		pagination: {
+			limit: 10,
+			page: 0,
+		},
+	});
+
+	const data = await readSettingsAction();
+
+	return (
+		<div className="container mx-auto py-10">
+			<div className="mb-8">
+				<h1 className="text-3xl font-bold tracking-tight">
+					Administrator Settings
+				</h1>
+				<p className="text-muted-foreground mt-2">
+					Manage system settings and monitor login activity.
+				</p>
+			</div>
+
+			<Tabs defaultValue="general" className="space-y-4">
+				<TabsList className="grid w-full grid-cols-2">
+					<TabsTrigger value="general">General Settings</TabsTrigger>
+					<TabsTrigger value="login">Login Attempts</TabsTrigger>
+				</TabsList>
+
+				<TabsContent value="general" className="space-y-4">
+					<GeneralSettings
+						data={{
+							description: data?.description || "",
+							isMaintenance: data?.isMaintenance || false,
+							websiteName: data?.websiteName || "",
+						}}
+					/>
+				</TabsContent>
+
+				<TabsContent value="login" className="space-y-4">
+					<LoginAttempts logs={loginLogs.data} />
+				</TabsContent>
+			</Tabs>
+		</div>
+	);
 }
