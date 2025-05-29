22cd8f74c5650b3889f3e8b007e060e4b4860349
awynx <alwin.puche16@gmail.com>
2025-05-29 21:23:17 +0800

    Add useDefaultColors prop to AlumniData component for conditional styling

--- src/app/admin/alumni/__components/alumni-data.tsx
+++ src/app/admin/alumni/__components/alumni-data.tsx
@@ -1,16 +1,20 @@
 export function AlumniData({
 	data,
 	label,
+	useDefaultColors,
 }: {
 	data?: string | null;
 	label: string;
+	useDefaultColors?: boolean;
 }) {
 	return (
 		<div>
 			<p className="text-sm">{label}</p>
 			<p
 				className={`${
-					data ? "font-medium" : "italic text-white/70 dark:text-white/70"
+					data
+						? "font-medium"
+						: !useDefaultColors && "italic text-white/70 dark:text-white/70"
 				} break-words`}>
 				{data || "No Data"}
 			</p>
