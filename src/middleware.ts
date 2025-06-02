053a28f814abc5eac5021396779631979a45101b
awynx <alwin.puche16@gmail.com>
2025-06-02 20:39:44 +0800

    Refactor import statements to consolidate email-related imports for improved organization

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
 import { cookies } from "next/headers";
-import { decrypt } from "@/lib";
+import { decrypt } from "@/lib/session";
 import {
 	adminRoutes,
 	alumniRoutes,
