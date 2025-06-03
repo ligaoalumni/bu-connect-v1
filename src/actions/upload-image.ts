e73e60939b5684cfd59db2427a1837d5067315b0
awynx <alwin.puche16@gmail.com>
2025-06-03 15:45:53 +0800

    Fix: Update import statement for Supabase client in upload-image action

--- src/actions/upload-image.ts
+++ src/actions/upload-image.ts
@@ -1,5 +1,5 @@
 "use server";
-import { createClient } from "@/lib";
+import { createClient } from "@/lib/supabase";
 import { v4 as uuidv4 } from "uuid";
 
 export const uploadImageAction = async (
