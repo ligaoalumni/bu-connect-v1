9fe2c83499c98122d8d71ec42ad192c86985b515
awynx <alwin.puche16@gmail.com>
2025-06-23 11:12:58 +0800

    Refactor profile edit form for improved formatting and type safety

--- src/app/(alumni)/profile/edit/page.tsx
+++ src/app/(alumni)/profile/edit/page.tsx
@@ -2,27 +2,25 @@ import { getInformation } from "@/actions";
 import EditProfileForm from "./__components/form";
 
 export default async function Page() {
-	const user = await getInformation();
+  const user = await getInformation();
 
-	if (!user) {
-		return <div>Loading...</div>;
-	}
+  if (!user) {
+    return <div>Loading...</div>;
+  }
 
-	const data = {
-		...user,
-		address:
-			Array.from(user.address as any).length > 0
-				? typeof user.address === "string"
-					? JSON.parse(user.address)
-					: user.address
-				: undefined,
-	};
+  const data = {
+    ...user,
+    address:
+      Array.from(user.address as any).length > 0
+        ? typeof user.address === "string"
+          ? JSON.parse(user.address)
+          : user.address
+        : undefined,
+  };
 
-	console.log(data, "qqq");
-
-	return (
-		<div>
-			<EditProfileForm user={data} />
-		</div>
-	);
+  return (
+    <div>
+      <EditProfileForm user={data} />
+    </div>
+  );
 }
