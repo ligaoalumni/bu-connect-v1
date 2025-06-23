c2a5ace943131059e8357a9d9e823ea9fdb9cd8a
awynx <alwin.puche16@gmail.com>
2025-06-23 11:16:26 +0800

    Format code with 2-space indentation and update UserTableData type

--- src/types/user.ts
+++ src/types/user.ts
@@ -3,57 +3,62 @@ import { AddressData } from "./index";
 
 export type UserRole = "ADMIN" | "SUPER_ADMIN" | "ALUMNI";
 
-export type UserTableData = Omit<User, "password" | "notifications">;
+export type UserTableData = Omit<
+  User,
+  "password" | "notifications" | "rate"
+> & {
+  rate?: number;
+};
 
 export interface UpdateProfileData {
-	address?: AddressData;
-	avatar?: string;
-	birthDate?: string;
-	company?: string;
-	contactNumber?: string;
-	firstName: string;
-	gender?: Gender;
-	lastName: string;
-	middleName?: string;
-	nationality?: string;
-	religion?: string;
+  address?: AddressData;
+  avatar?: string;
+  birthDate?: string;
+  company?: string;
+  contactNumber?: string;
+  firstName: string;
+  gender?: Gender;
+  lastName: string;
+  middleName?: string;
+  nationality?: string;
+  religion?: string;
 
-	batch?: number;
+  batch?: number;
 
-	jobTitle?: string;
-	course?: string;
-	currentOccupation?: string;
+  jobTitle?: string;
+  course?: string;
+  currentOccupation?: string;
 }
 
 export type Gender = "MALE" | "FEMALE" | "PREFER_NOT_TO_SAY";
 
 export type AlumniDataTableColumns = Pick<
-	User,
-	| "id"
-	| "studentId"
-	| "firstName"
-	| "lastName"
-	| "middleName"
-	| "email"
-	| "contactNumber"
-	| "address"
-	| "birthDate"
-	| "batch"
-	| "course"
-	| "gender"
-	| "status"
+  User,
+  | "id"
+  | "studentId"
+  | "firstName"
+  | "lastName"
+  | "middleName"
+  | "email"
+  | "contactNumber"
+  | "address"
+  | "birthDate"
+  | "batch"
+  | "course"
+  | "gender"
+  | "status"
 >;
 
 export type UpdateUserArgs = Partial<
-	Pick<
-		User,
-		| "avatar"
-		| "firstName"
-		| "lastName"
-		| "middleName"
-		| "verifiedAt"
-		| "shareLocation"
-		| "occupationStatus"
-		| "rate"
-	>
+  Pick<
+    User,
+    | "avatar"
+    | "firstName"
+    | "lastName"
+    | "middleName"
+    | "verifiedAt"
+    | "shareLocation"
+    | "occupationStatus"
+    | "rate"
+  >
 >;
