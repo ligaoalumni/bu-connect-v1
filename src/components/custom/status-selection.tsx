ef2cbc3a57776754480a78117b0a56a4435f1970
awynx <alwin.puche16@gmail.com>
2025-06-24 20:47:26 +0800

    Refactor status selection to navigate on change instead of updating user directly

--- src/components/custom/status-selection.tsx
+++ src/components/custom/status-selection.tsx
@@ -1,91 +1,91 @@
 "use client";
 
-import { updateUserAction } from "@/actions";
 import {
-	Select,
-	SelectContent,
-	SelectGroup,
-	SelectItem,
-	SelectLabel,
-	SelectTrigger,
-	SelectValue,
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
 } from "@/components";
 import { getOccupationStatusLabel } from "@/lib";
 import { OccupationStatus } from "@prisma/client";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
-import { toast } from "sonner";
 
 // Transform OccupationStatus into an array
 const occupationStatusArray = Object.values(
-	OccupationStatus
+  OccupationStatus,
 ) as OccupationStatus[];
 
 interface StatusSelectionProps {
-	initialValue?: OccupationStatus;
+  initialValue?: OccupationStatus;
 }
 
 export function StatusSelection({ initialValue }: StatusSelectionProps) {
-	const [selectedStatus, setSelectedStatus] = useState<
-		OccupationStatus | undefined
-	>(initialValue);
-	const [loading, setLoading] = useState(false);
+  const [selectedStatus, setSelectedStatus] = useState<
+    OccupationStatus | undefined
+  >(initialValue);
+  const router = useRouter();
 
-	// Handle onChange event
-	const handleChange = async (value: OccupationStatus) => {
-		const prevValue = selectedStatus;
+  // Handle onChange event
+  const handleChange = async (value: OccupationStatus) => {
+    // const prevValue = selectedStatus;
 
-		try {
-			setLoading(true);
-			setSelectedStatus(value);
+    setSelectedStatus(value);
+    return router.push(`/update-status?status=${value}`);
 
-			await updateUserAction({
-				occupationStatus: value,
-			});
-			toast.success("Occupation status updated successfully!", {
-				duration: 5000,
-				position: "top-center",
-				richColors: true,
-				description: `Your occupation status is now set to ${getOccupationStatusLabel(
-					value
-				)}.`,
-			});
-		} catch (error) {
-			toast.error("Failed to update occupation status.", {
-				duration: 5000,
-				position: "top-center",
-				richColors: true,
-				description: "Please try again later.",
-			});
-			console.log(error);
-			setSelectedStatus(prevValue);
-		} finally {
-			setLoading(false);
-		}
-	};
+    // try {
+    //   setLoading(true);
+    //   setSelectedStatus(value);
 
-	return (
-		<Select
-			disabled={loading}
-			value={selectedStatus}
-			onValueChange={handleChange}>
-			<SelectTrigger className="w-full">
-				<SelectValue placeholder="Select a status">
-					{selectedStatus
-						? getOccupationStatusLabel(selectedStatus)
-						: "Select a status"}
-				</SelectValue>
-			</SelectTrigger>
+    //   await updateUserAction({
+    //     occupationStatus: value,
+    //   });
 
-			<SelectContent>
-				<SelectGroup>
-					<SelectLabel>Occupation Status</SelectLabel>
-					{occupationStatusArray.map((status) => (
-						<SelectItem key={status} value={status}>
-							{getOccupationStatusLabel(status)}
-						</SelectItem>
-					))}
-				</SelectGroup>
-			</SelectContent>
-		</Select>
-	);
+    //   toast.success("Occupation status updated successfully!", {
+    //     duration: 5000,
+    //     position: "top-center",
+    //     richColors: true,
+    //     description: `Your occupation status is now set to ${getOccupationStatusLabel(
+    //       value,
+    //     )}.`,
+    //   });
+    // } catch (error) {
+    //   toast.error("Failed to update occupation status.", {
+    //     duration: 5000,
+    //     position: "top-center",
+    //     richColors: true,
+    //     description: "Please try again later.",
+    //   });
+    //   console.log(error);
+    //   setSelectedStatus(prevValue);
+    // } finally {
+    //   setLoading(false);
+    // }
+  };
+
+  return (
+    <Select value={selectedStatus} onValueChange={handleChange}>
+      <SelectTrigger className="w-full">
+        <SelectValue placeholder="Select a status">
+          {selectedStatus
+            ? getOccupationStatusLabel(selectedStatus)
+            : "Select a status"}
+        </SelectValue>
+      </SelectTrigger>
+
+      <SelectContent>
+        <SelectGroup>
+          <SelectLabel>Occupation Status</SelectLabel>
+          {occupationStatusArray.map((status) => (
+            <SelectItem key={status} value={status}>
+              {getOccupationStatusLabel(status)}
+            </SelectItem>
+          ))}
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  );
 }
