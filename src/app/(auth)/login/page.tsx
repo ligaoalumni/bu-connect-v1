e7dfe7e2b1e4a84f93b118185ebcb5ff1607a332
awynx <alwin.puche16@gmail.com>
2025-08-06 11:48:44 +0800

    remove social integration UI

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -3,19 +3,17 @@ import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import {
-	Form,
-	FormControl,
-	FormField,
-	FormItem,
-	FormLabel,
-	FormMessage,
-	Input,
-	Button,
-	Card,
-	CardContent,
-	CardFooter,
-	Checkbox,
-	Iconify,
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  Input,
+  Button,
+  Card,
+  CardContent,
+  Checkbox,
 } from "@/components";
 import { Lock, Mail } from "lucide-react";
 import { LoginFormSchema } from "@/lib";
@@ -28,195 +26,172 @@ import Link from "next/link";
 import Approval from "../__components/approval";
 
 const LoginForm = () => {
-	const router = useRouter();
-	const [isPending, setIsPending] = useState(false);
-	const form = useForm<LoginFormData>({
-		resolver: zodResolver(LoginFormSchema),
-		defaultValues: {
-			email: "",
-			password: "",
-			rememberMe: false,
-		},
-	});
+  const router = useRouter();
+  const [isPending, setIsPending] = useState(false);
+  const form = useForm<LoginFormData>({
+    resolver: zodResolver(LoginFormSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      rememberMe: false,
+    },
+  });
 
-	const onSubmit = async (data: LoginFormData) => {
-		let success = false;
-		try {
-			const response = await loginAction(data.email, data.password);
+  const onSubmit = async (data: LoginFormData) => {
+    let success = false;
+    try {
+      const response = await loginAction(data.email, data.password);
 
-			if (response?.error.message) {
-				if (response.error.isPending) {
-					setIsPending(true);
-				}
+      if (response?.error.message) {
+        if (response.error.isPending) {
+          setIsPending(true);
+        }
 
-				throw new Error(response.error.message);
-			}
+        throw new Error(response.error.message);
+      }
 
-			await revalidatePathAction("/");
-			success = true;
-			toast.success("Success", {
-				description: "Welcome back!",
-				position: "top-center",
-				richColors: true,
-				duration: 5000,
-			});
-		} catch (err) {
-			success = false;
+      await revalidatePathAction("/");
+      success = true;
+      toast.success("Success", {
+        description: "Welcome back!",
+        position: "top-center",
+        richColors: true,
+        duration: 5000,
+      });
+    } catch (err) {
+      success = false;
 
-			toast.error("Log in Error", {
-				description: (err as Error).message,
-				position: "top-center",
-				richColors: true,
-				duration: 5000,
-			});
-		}
-		if (success) {
-			router.replace("/");
-		}
-	};
+      toast.error("Log in Error", {
+        description: (err as Error).message,
+        position: "top-center",
+        richColors: true,
+        duration: 5000,
+      });
+    }
+    if (success) {
+      router.replace("/");
+    }
+  };
 
-	const handleApprovalClick = () => {
-		setIsPending(false);
-		form.reset();
-	};
+  const handleApprovalClick = () => {
+    setIsPending(false);
+    form.reset();
+  };
 
-	return (
-		<div className=" bg-[#94949440]  bg-cover bg-center rounded-[0.7rem] ring-0 ring-[#949494] bg-opacity-20   ">
-			<Card className="w-full md:min-w-[400px] pt-16 max-w-md mx-auto border-none bg-transparent relative">
-				<Image
-					src="/images/bup-logo.png"
-					height={120}
-					width={120}
-					alt="LNHS Logo"
-					className="absolute -translate-y-[105%] -translate-x-[50%] left-[50%]"
-				/>
+  return (
+    <div className=" bg-[#94949440]  bg-cover bg-center rounded-[0.7rem] ring-0 ring-[#949494] bg-opacity-20   ">
+      <Card className="w-full md:min-w-[400px] pt-[4.15rem] max-w-md mx-auto border-none bg-transparent relative">
+        <Image
+          src="/images/bup-logo.png"
+          height={120}
+          width={120}
+          alt="LNHS Logo"
+          className="absolute -translate-y-[105%] -translate-x-[50%] left-[50%]"
+        />
 
-				{isPending ? (
-					<div className="p-5">
-						<Approval handleClick={handleApprovalClick} />
-					</div>
-				) : (
-					<>
-						<CardContent>
-							<Form {...form}>
-								<form
-									onSubmit={form.handleSubmit(onSubmit)}
-									className="space-y-4">
-									<FormField
-										control={form.control}
-										name="email"
-										render={({ field }) => (
-											<FormItem>
-												<FormControl>
-													<div className="relative mt-8">
-														<Mail className="absolute left-2 top-[50%] translate-y-[-50%] h-4 w-4 text-muted-foreground" />
-														<Input
-															readOnly={form.formState.isSubmitting}
-															placeholder="Enter email address"
-															className="pl-8 bg-white dark:text-black  dark:selection:bg-black/15 border-none h-10 rounded-none"
-															{...field}
-														/>
-													</div>
-												</FormControl>
-												<FormMessage />
-											</FormItem>
-										)}
-									/>
+        {isPending ? (
+          <div className="p-5">
+            <Approval handleClick={handleApprovalClick} />
+          </div>
+        ) : (
+          <>
+            <CardContent>
+              <Form {...form}>
+                <form
+                  onSubmit={form.handleSubmit(onSubmit)}
+                  className="space-y-4"
+                >
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormControl>
+                          <div className="relative mt-6">
+                            <Mail className="absolute left-2 top-[50%] translate-y-[-50%] h-4 w-4 text-muted-foreground" />
+                            <Input
+                              readOnly={form.formState.isSubmitting}
+                              placeholder="Enter email address"
+                              className="pl-8 bg-white dark:text-black  dark:selection:bg-black/15 border-none h-10 rounded-none"
+                              {...field}
+                            />
+                          </div>
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
 
-									<FormField
-										control={form.control}
-										name="password"
-										render={({ field }) => (
-											<FormItem>
-												<FormControl>
-													<div className="relative ">
-														<Lock className="absolute left-2 top-[50%] translate-y-[-50%] h-4 w-4 text-muted-foreground" />
-														<Input
-															readOnly={form.formState.isSubmitting}
-															type="password"
-															placeholder="Enter password"
-															className="pl-8 rounded-none dark:text-black dark:selection:bg-black/15 bg-white border-none h-10"
-															{...field}
-														/>
-													</div>
-												</FormControl>
-												<FormMessage />
-											</FormItem>
-										)}
-									/>
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormControl>
+                          <div className="relative ">
+                            <Lock className="absolute left-2 top-[50%] translate-y-[-50%] h-4 w-4 text-muted-foreground" />
+                            <Input
+                              readOnly={form.formState.isSubmitting}
+                              type="password"
+                              placeholder="Enter password"
+                              className="pl-8 rounded-none dark:text-black dark:selection:bg-black/15 bg-white border-none h-10"
+                              {...field}
+                            />
+                          </div>
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
 
-									<div className="flex items-center justify-between">
-										<div>
-											<FormField
-												control={form.control}
-												name="rememberMe"
-												render={({ field }) => (
-													<FormItem>
-														<FormControl>
-															<div className="flex gap-2">
-																<Checkbox
-																	className="rounded-none data-[state=checked]:text-white dark:text-black  border-black data-[state=checked]:bg-black"
-																	onCheckedChange={field.onChange}
-																	type="button"
-																/>
-																<FormLabel className="text-white dark:text-black ">
-																	Remember me
-																</FormLabel>
-															</div>
-														</FormControl>
-													</FormItem>
-												)}
-											/>
-										</div>
-										<Button
-											variant="link"
-											className="px-0 font-normal text-white    italic"
-											type="button"
-											asChild>
-											<Link href="/forgot-password">Forgot password?</Link>
-										</Button>
-									</div>
+                  <div className="flex items-center justify-between mt-2">
+                    <div>
+                      <FormField
+                        control={form.control}
+                        name="rememberMe"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormControl>
+                              <div className="flex gap-2">
+                                <Checkbox
+                                  className="rounded-none data-[state=checked]:text-white dark:text-black  border-black data-[state=checked]:bg-black"
+                                  onCheckedChange={field.onChange}
+                                  type="button"
+                                />
+                                <FormLabel className="text-white dark:text-black ">
+                                  Remember me
+                                </FormLabel>
+                              </div>
+                            </FormControl>
+                          </FormItem>
+                        )}
+                      />
+                    </div>
+                    <Button
+                      variant="link"
+                      className="px-0 font-normal text-white    italic"
+                      type="button"
+                      asChild
+                    >
+                      <Link href="/forgot-password">Forgot password?</Link>
+                    </Button>
+                  </div>
 
-									<Button
-										type="submit"
-										className="w-full bg-[#FF9500] hover:bg-[#FF9500]"
-										disabled={form.formState.isSubmitting}>
-										{form.formState.isSubmitting ? "Logging in..." : "Login"}
-									</Button>
-								</form>
-							</Form>
-						</CardContent>
-
-						<CardFooter className="flex flex-col items-center gap-y-3 justify-center">
-							<p className="text-sm text-white ">or Sign in with</p>
-							<div className="flex gap-5">
-								<Link className="" href="#">
-									<Iconify icon="logos:facebook" width="32" height="32" />
-								</Link>
-								<Link className="" href="#">
-									<Iconify
-										icon="skill-icons:instagram"
-										width="32"
-										height="32"
-									/>
-								</Link>
-								<Link className="" href="#">
-									<Iconify icon="skill-icons:linkedin" width="32" height="32" />
-								</Link>
-								<Link className="" href="#">
-									<Iconify
-										icon="flat-color-icons:google"
-										width="32"
-										height="32"
-									/>
-								</Link>
-							</div>
-						</CardFooter>
-					</>
-				)}
-			</Card>
-		</div>
-	);
+                  <Button
+                    type="submit"
+                    className="w-full mt-2 bg-[#FF9500] hover:bg-[#FF9500]"
+                    disabled={form.formState.isSubmitting}
+                  >
+                    {form.formState.isSubmitting ? "Logging in..." : "Login"}
+                  </Button>
+                </form>
+              </Form>
+            </CardContent>
+          </>
+        )}
+      </Card>
+    </div>
+  );
 };
 
 export default LoginForm;
--- src/components/custom/hero-section.tsx
+++ src/components/custom/hero-section.tsx
@@ -4,44 +4,44 @@ import Image from "next/image";
 import React from "react";
 import { Button } from "../ui/button";
 import {
-	IconBrandFacebook,
-	IconBrandGoogle,
-	IconBrandLinkedin,
+  IconBrandFacebook,
+  IconBrandGoogle,
+  IconBrandLinkedin,
 } from "@tabler/icons-react";
 import Link from "next/link";
 import { useAuth } from "@/contexts";
 
 export function HeroSection() {
-	const { user } = useAuth();
-	return (
-		<section className="px-5 md:px-0">
-			<div className="container    mx-auto min-h-[500px] h-full md:min-h-[664px] relative">
-				<Image
-					alt="BUP Hero Image"
-					className="absolute z-10 "
-					src="/images/bup-hero-img.png"
-					fill
-					objectFit="cover"
-				/>
-				<div className="absolute space-y-5 px-5 z-20 h-full w-full  flex flex-col justify-center left-0 bg-[#15497A85]">
-					<h1 className="font-bold   max text-white text-2xl md:text-6xl text-left max-w-[90dvw] md:max-w-[40dvw]  ">
-						Strengthening Alumni Ties Through Seamless Digital Engagement
-					</h1>
-					<p className="text-white text-lg md:text-xl max-w-[80dvw] md:max-w-[30dvw]  ">
-						Connect with fellow BU Polangui alumni, share memories, and be
-						updated on news and events
-					</p>
-					{user === null && (
-						<div className="flex items-center gap-4">
-							<Button asChild className="bg-[#E6750C] hover:bg-[#E6750C90]">
-								<Link href="/register">Register</Link>
-							</Button>
-							<Button asChild className="bg-white text-black">
-								<Link href="/login">Log in</Link>
-							</Button>
-						</div>
-					)}
-					<div className="flex items-center gap-2">
+  const { user } = useAuth();
+  return (
+    <section className="px-5 md:px-0">
+      <div className="container    mx-auto min-h-[500px] h-full md:min-h-[664px] relative">
+        <Image
+          alt="BUP Hero Image"
+          className="absolute z-10 "
+          src="/images/bup-hero-img.png"
+          fill
+          objectFit="cover"
+        />
+        <div className="absolute space-y-5 px-5 z-20 h-full w-full  flex flex-col justify-center left-0 bg-[#15497A85]">
+          <h1 className="font-bold   max text-white text-2xl md:text-6xl text-left max-w-[90dvw] md:max-w-[40dvw]  ">
+            Strengthening Alumni Ties Through Seamless Digital Engagement
+          </h1>
+          <p className="text-white text-lg md:text-xl max-w-[80dvw] md:max-w-[30dvw]  ">
+            Connect with fellow BU Polangui alumni, share memories, and be
+            updated on news and events
+          </p>
+          {user === null && (
+            <div className="flex items-center gap-4">
+              <Button asChild className="bg-[#E6750C] hover:bg-[#E6750C90]">
+                <Link href="/register">Register</Link>
+              </Button>
+              <Button asChild className="bg-white text-black">
+                <Link href="/login">Log in</Link>
+              </Button>
+            </div>
+          )}
+          {/*<div className="flex items-center gap-2">
 						<Button
 							className="rounded-full bg-white hover:bg-white/80 "
 							size="icon">
@@ -57,9 +57,9 @@ export function HeroSection() {
 							size="icon">
 							<IconBrandLinkedin className="stroke-[#E6750C] " />
 						</Button>
-					</div>
-				</div>
-			</div>
-		</section>
-	);
+					</div>*/}
+        </div>
+      </div>
+    </section>
+  );
 }
