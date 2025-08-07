26464d0da785159e3558c6c8de606db514d309d1
awynx <alwin.puche16@gmail.com>
2025-08-08 07:07:28 +0800

    add constant options

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -3,19 +3,20 @@ import { Job, User } from "@prisma/client";
 
 export * from "./blur-data";
 export * from "./email-templates";
+export * from "./options";
 
 // 1. Specify protected and public routes
 export const adminRoutes = [
-	"/admin",
-	"/admin/alumni",
-	"/admin/alumni/records",
-	"/admin/alumni/records/add",
-	"/admin/events",
-	"/admin/events/add-new-event",
-	"/admin/settings",
-	"/admin/account-info",
-	"/admin/announcements",
-	"/admin/announcements/add",
+  "/admin",
+  "/admin/alumni",
+  "/admin/alumni/records",
+  "/admin/alumni/records/add",
+  "/admin/events",
+  "/admin/events/add-new-event",
+  "/admin/settings",
+  "/admin/account-info",
+  "/admin/announcements",
+  "/admin/announcements/add",
 ];
 
 export const sharedRoutes = ["/verify-account"];
@@ -28,61 +29,61 @@ export const authRoutes = ["/login", "/signup"];
 export const publicRoutes = alumniRoutes.concat(["/"]);
 
 export const eventStatusColorMap: Record<
-	ReturnType<typeof getEventStatus>,
-	"default" | "destructive" | "secondary" | "outline"
+  ReturnType<typeof getEventStatus>,
+  "default" | "destructive" | "secondary" | "outline"
 > = {
-	"Ongoing Event": "secondary",
-	"Upcoming Event": "default",
-	"Past Event": "secondary",
-	"Unknown Status": "destructive",
+  "Ongoing Event": "secondary",
+  "Upcoming Event": "default",
+  "Past Event": "secondary",
+  "Unknown Status": "destructive",
 };
 
 export const userStatusColorMap: Record<
-	User["status"],
-	"default" | "destructive" | "secondary" | "outline"
+  User["status"],
+  "default" | "destructive" | "secondary" | "outline"
 > = {
-	ACTIVE: "default",
-	BLOCKED: "destructive",
-	PENDING: "outline",
-	DELETED: "destructive",
+  ACTIVE: "default",
+  BLOCKED: "destructive",
+  PENDING: "outline",
+  DELETED: "destructive",
 };
 
 export const seniorHighStrands = [
-	"NA - Not Applicable",
-	"ACADEMIC - ABM",
-	"ACADEMIC - HUMSS",
-	"ACADEMIC - STEM",
-	"ACADEMIC - GAS",
-	"TVL - ICT",
-	"TVL - Home Economics",
-	"TVL - Agri-Fishery Arts",
-	"TVL - Industrial Arts",
-	"SPORTS - Sports Track",
-	"ARTS AND DESIGN - Arts and Design Track",
+  "NA - Not Applicable",
+  "ACADEMIC - ABM",
+  "ACADEMIC - HUMSS",
+  "ACADEMIC - STEM",
+  "ACADEMIC - GAS",
+  "TVL - ICT",
+  "TVL - Home Economics",
+  "TVL - Agri-Fishery Arts",
+  "TVL - Industrial Arts",
+  "SPORTS - Sports Track",
+  "ARTS AND DESIGN - Arts and Design Track",
 ];
 
 export const alumniLabel: Record<User["gender"], string> = {
-	FEMALE: "Alumna",
-	MALE: "Alumnus",
-	PREFER_NOT_TO_SAY: "Alumnus",
+  FEMALE: "Alumna",
+  MALE: "Alumnus",
+  PREFER_NOT_TO_SAY: "Alumnus",
 };
 
 export const jobTypes = ["FULL_TIME", "PART_TIME", "INTERNSHIP", "FREELANCE"];
 
 export const JobStatusBadgeColorMap: Record<
-	Job["status"],
-	"default" | "destructive" | "secondary" | "outline"
+  Job["status"],
+  "default" | "destructive" | "secondary" | "outline"
 > = {
-	CLOSED: "destructive",
-	OPEN: "secondary",
-	COMPLETED: "default",
+  CLOSED: "destructive",
+  OPEN: "secondary",
+  COMPLETED: "default",
 };
 
 export const INDUSTRIES = [
-	{ id: "tech", name: "Technology" },
-	{ id: "finance", name: "Finance" },
-	{ id: "healthcare", name: "Healthcare" },
-	{ id: "education", name: "Education" },
-	{ id: "manufacturing", name: "Manufacturing" },
-	{ id: "retail", name: "Retail" },
+  { id: "tech", name: "Technology" },
+  { id: "finance", name: "Finance" },
+  { id: "healthcare", name: "Healthcare" },
+  { id: "education", name: "Education" },
+  { id: "manufacturing", name: "Manufacturing" },
+  { id: "retail", name: "Retail" },
 ];
