68588d41e3c617a24306e8432a6e9b5e87df0bbc
awynx <alwin.puche16@gmail.com>
2025-08-28 22:19:45 +0800

    Refactor old alumni form

--- src/app/admin/alumni/__components/add-old-alumni-form.tsx
+++ src/app/admin/alumni/__components/add-old-alumni-form.tsx
@@ -1,10 +1,18 @@
 "use client";
 
-import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
-import { Users } from "lucide-react";
-
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import {
   Form,
   FormControl,
@@ -12,26 +20,30 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-  Input,
-  Button,
+} from "@/components/ui/form";
+import {
   Select,
-  SelectTrigger,
-  SelectValue,
   SelectContent,
-  SelectLabel,
   SelectGroup,
   SelectItem,
-  Card,
-  CardHeader,
-  CardTitle,
-  CardDescription,
-  CardContent,
-  CardFooter,
-} from "@/components";
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { programs } from "@/constant";
-import { DatePicker } from "./date-picker";
+import { OldAccount } from "@prisma/client";
+import { useState } from "react";
+import { TabsContent, TabsList, TabsTrigger } from "@/components";
+import { Tabs } from "@radix-ui/react-tabs";
+import CSVUploader from "../../_components/csv-uploader";
+import { createOldAccountAction } from "@/actions";
+import { toast } from "sonner";
 
 const formSchema = z.object({
+  studentID: z
+    .string()
+    .min(1, "Student ID is required")
+    .max(20, "Student ID must be less than 20 characters"),
   firstName: z
     .string()
     .min(1, "First name is required")
@@ -44,192 +56,278 @@ const formSchema = z.object({
     .string()
     .max(50, "Middle name must be less than 50 characters")
     .optional(),
-  birthDate: z.date({
-    required_error: "Birth date is required",
-  }),
-  program: z
-    .string()
-    .min(1, "Program is required")
-    .max(100, "Program must be less than 100 characters"),
-  batch: z.coerce.number().int().min(1, "Batch must be a positive number"),
+  birthDate: z.string().min(1, "Birth date is required"),
+  program: z.string().min(1, "Program is required"),
+  batch: z.string().min(1, "Batch is required"),
 });
 
-export type OldAlumniData = z.infer<typeof formSchema>;
+type OldAccountFormData = z.infer<typeof formSchema>;
 
-interface AddOldAlumniRecordFormProps {
-  initialData?: Partial<OldAlumniData>;
+interface OldAccountModalProps {
+  initialValue?: OldAccount;
 }
 
 const currentYear = new Date().getFullYear();
-const years = Array.from({ length: 70 }, (_, i) => currentYear - i);
+const years = Array.from({ length: 10 }, (_, i) => currentYear - i);
 
-export function AddOldAlumniRecordForm({
-  initialData,
-}: AddOldAlumniRecordFormProps) {
-  const form = useForm<OldAlumniData>({
+export function OldAccountModal({ initialValue }: OldAccountModalProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  const form = useForm<OldAccountFormData>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      firstName: initialData?.firstName || "",
-      lastName: initialData?.lastName || "",
-      middleName: initialData?.middleName || "",
-      birthDate: initialData?.birthDate || undefined,
-      program: initialData?.program || "",
-      batch: initialData?.batch || 1,
+      studentID: "",
+      firstName: "",
+      lastName: "",
+      middleName: "",
+      program: "",
+      batch: "",
     },
   });
 
-  const handleSubmit = async (data: OldAlumniData) => {};
+  const handleOnOpenChange = () => setIsOpen((state) => !state);
 
-  return (
-    <Card>
-      <CardHeader>
-        <CardTitle className="flex items-center gap-2">
-          <Users className="h-5 w-5" />
-          {initialData ? "Edit" : "Add"} Old Alumni Data
-        </CardTitle>
-        <CardDescription>
-          {initialData
-            ? "Edit student information to update the system."
-            : "Enter student information to add a single student to the system."}
-        </CardDescription>
-      </CardHeader>
+  const onSubmit = async (data: OldAccountFormData) => {
+    // console.log("Student data:", data);
+    // // Handle form submission here
 
+    // handleOnOpenChange();
+    // form.reset();
+
+    try {
+      if (initialValue) {
+      } else {
+        await createOldAccountAction({
+          ...data,
+          middleName: data.middleName || "",
+          batch: Number(data.batch),
+          birthDate: new Date(data.birthDate),
+        });
+      }
+
+      toast.success(
+        initialValue
+          ? "Old account updated successfully!"
+          : "Old account created successfully!",
+        {
+          description: `The old account information has been saved.`,
+          position: "top-center",
+          richColors: true,
+        },
+      );
+      handleOnOpenChange();
+      form.reset();
+    } catch (error) {
+      console.log(`Error: ${error}`);
+      toast.error("Something went wrong!", {
+        description: "An error occurred while creating the old account.",
+        position: "top-center",
+        richColors: true,
+      });
+    }
+  };
+
+  const FormComponent = () => {
+    return (
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(handleSubmit)}>
-          <CardContent className="space-y-6">
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-              <FormField
-                control={form.control}
-                name="firstName"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>First Name</FormLabel>
-                    <FormControl>
-                      <Input placeholder="Enter first name" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+          <div className="grid grid-cols-2 gap-4">
+            <FormField
+              control={form.control}
+              name="studentID"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Student ID *</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter student ID" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-              <FormField
-                control={form.control}
-                name="lastName"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Last Name</FormLabel>
+            <FormField
+              control={form.control}
+              name="batch"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Batch *</FormLabel>
+                  <Select onValueChange={field.onChange} value={field.value}>
                     <FormControl>
-                      <Input placeholder="Enter last name" {...field} />
+                      <SelectTrigger>
+                        <SelectValue placeholder="Select student's batch" />
+                      </SelectTrigger>
                     </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-              <FormField
-                control={form.control}
-                name="middleName"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Middle Name (Optional)</FormLabel>
-                    <FormControl>
-                      <Input placeholder="Enter middle name" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+                    <SelectContent>
+                      <SelectGroup>
+                        <SelectLabel>Batch</SelectLabel>
+                        {years.map((batch) => (
+                          <SelectItem value={batch.toString()} key={batch}>
+                            {batch.toString()}
+                          </SelectItem>
+                        ))}
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
 
-              <FormField
-                control={form.control}
-                name="birthDate"
-                render={({ field }) => (
-                  <FormItem className="flex flex-col">
-                    <FormLabel>Birth Date</FormLabel>
-                    <DatePicker value={field.value} onChange={field.onChange} />
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-              <FormField
-                control={form.control}
-                name="program"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Program</FormLabel>
-                    <FormControl>
-                      <Select
-                        onValueChange={field.onChange}
-                        value={field.value.toString()}
-                      >
-                        {/* Option to prompt user */}
-                        <SelectTrigger className="   ">
-                          <SelectValue placeholder="Select student's program" />
-                        </SelectTrigger>
-                        <SelectContent>
-                          <SelectGroup>
-                            <SelectLabel>Program</SelectLabel>
-                            {programs.map((program) => (
-                              <SelectItem value={program} key={program}>
-                                {program}
-                              </SelectItem>
-                            ))}
-                          </SelectGroup>
-                        </SelectContent>
-                      </Select>
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+          <div className="grid grid-cols-2 gap-4">
+            <FormField
+              control={form.control}
+              name="firstName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>First Name *</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter first name" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-              <FormField
-                control={form.control}
-                name="batch"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Batch</FormLabel>
-                    <FormControl>
-                      <Select
-                        onValueChange={field.onChange}
-                        value={field.value.toString()}
-                      >
-                        <SelectTrigger className="w-full  ">
-                          <SelectValue placeholder="Select alumnus/alumna batch" />
-                        </SelectTrigger>
-                        <SelectContent>
-                          <SelectGroup>
-                            <SelectLabel>Batch</SelectLabel>
-                            {years.map((batch) => (
-                              <SelectItem value={batch.toString()} key={batch}>
-                                {batch.toString()}
-                              </SelectItem>
-                            ))}
-                          </SelectGroup>
-                        </SelectContent>
-                      </Select>
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-          </CardContent>
+            <FormField
+              control={form.control}
+              name="lastName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Last Name *</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter last name" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
 
-          <CardFooter>
+          <FormField
+            control={form.control}
+            name="middleName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Middle Name</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Enter middle name (optional)"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="birthDate"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Birth Date *</FormLabel>
+                <FormControl>
+                  {/*<DatePicker value={field.value} onChange={field.onChange} />*/}
+                  <Input type="date" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="program"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Program *</FormLabel>
+                <Select onValueChange={field.onChange} value={field.value}>
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select student's program" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectLabel>Program</SelectLabel>
+                      {programs.map((program) => (
+                        <SelectItem value={program} key={program}>
+                          {program}
+                        </SelectItem>
+                      ))}
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <div className="flex justify-end gap-3 pt-4">
             <Button
-              type="submit"
-              disabled={form.formState.isSubmitting}
-              className="ml-auto min-w-72 "
+              type="button"
+              variant="outline"
+              onClick={handleOnOpenChange}
             >
-              {form.formState.isSubmitting ? "Submitting..." : "Submit"}
+              Cancel
             </Button>
-          </CardFooter>
+            <Button type="submit">Add Student</Button>
+          </div>
         </form>
       </Form>
-    </Card>
+    );
+  };
+
+  return (
+    <Dialog open={isOpen}>
+      <DialogTrigger asChild onClick={handleOnOpenChange}>
+        <Button variant="outline">Add Old Alumni</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[500px]">
+        {initialValue ? (
+          <>
+            <DialogHeader>
+              <DialogTitle>Edit Data</DialogTitle>
+              <DialogDescription>
+                Manually input or upload historical data that was collected
+                before the current system was implemented.
+              </DialogDescription>
+            </DialogHeader>
+            <hr className="my-2" />
+            <FormComponent />
+          </>
+        ) : (
+          <Tabs defaultValue="manual" className="w-full">
+            <TabsList className="grid w-full grid-cols-2">
+              <TabsTrigger value="manual">Manual</TabsTrigger>
+              <TabsTrigger value="automatic">Batch Upload</TabsTrigger>
+            </TabsList>
+            <TabsContent value="manual">
+              <DialogHeader>
+                <DialogTitle>Add Old Data</DialogTitle>
+                <DialogDescription>
+                  Manually input or upload historical data that was collected
+                  before the current system was implemented.
+                </DialogDescription>
+              </DialogHeader>
+              <hr className="my-2" />
+              <FormComponent />
+            </TabsContent>
+            <TabsContent value="automatic">
+              <DialogHeader className="mb-4">
+                <DialogTitle>Upload Batch Data</DialogTitle>
+                <DialogDescription>
+                  Upload a CSV or Excel file to add multiple records at once.
+                  Make sure your data follows the required format.
+                </DialogDescription>
+              </DialogHeader>
+              <hr className="mb-4" />
+              <CSVUploader isOldAlumni />
+            </TabsContent>
+          </Tabs>
+        )}
+      </DialogContent>
+    </Dialog>
   );
 }
