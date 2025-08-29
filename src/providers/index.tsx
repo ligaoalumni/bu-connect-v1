91710cc467bb49ea13c2286be93e1e96867ca2a6
awynx <alwin.puche16@gmail.com>
2025-08-29 15:36:59 +0800

    Wrap providers with GoogleOAuthProvider

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -4,23 +4,29 @@ import { ThemeProvider } from "./theme-provider";
 import NextTopLoader from "nextjs-toploader";
 import { Toaster } from "@/components";
 import { AuthProvider, ContentProvider } from "@/contexts";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-	return (
-		<>
-			<AuthProvider>
-				<ContentProvider>
-					<NextTopLoader showSpinner={false} speed={1000} />
-					<ThemeProvider
-						attribute="class"
-						defaultTheme="light"
-						enableSystem
-						disableTransitionOnChange>
-						{children}
-						<Toaster />
-					</ThemeProvider>
-				</ContentProvider>
-			</AuthProvider>
-		</>
-	);
+  return (
+    <>
+      <GoogleOAuthProvider
+        clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || ""}
+      >
+        <AuthProvider>
+          <ContentProvider>
+            <NextTopLoader showSpinner={false} speed={1000} />
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="light"
+              enableSystem
+              disableTransitionOnChange
+            >
+              {children}
+              <Toaster />
+            </ThemeProvider>
+          </ContentProvider>
+        </AuthProvider>
+      </GoogleOAuthProvider>
+    </>
+  );
 }
