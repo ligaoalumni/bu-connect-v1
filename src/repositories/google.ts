94fdd3fe91992614961a58b7ebf65bd28abbec32
awynx <alwin.puche16@gmail.com>
2025-09-04 22:52:04 +0800

    Remove unused imports and parameters from Google login

--- src/repositories/google.ts
+++ src/repositories/google.ts
@@ -1,14 +1,9 @@
 "use server";
 
 import { prisma } from "@/lib";
-import { redirect } from "next/navigation";
-import { logLoginAttempt } from "./login-logs";
 
 export const loginWithGoogle = async ({
   email,
-  firstName,
-  lastName,
-  photo,
 }: {
   email: string;
   photo?: string;
