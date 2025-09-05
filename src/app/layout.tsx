c324f042c6e89e6ebc1a13d1a0a4fdc31a06a57f
awynx <alwin.puche16@gmail.com>
2025-09-05 08:34:38 +0800

    refactor document page title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,63 +7,64 @@ import { getInformation, readSettingsAction } from "@/actions";
 import MaintenancePage from "./maintenance/page";
 
 const geistSans = Geist({
-	variable: "--font-geist-sans",
-	subsets: ["latin"],
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
 });
 
 const poppins = Poppins({
-	variable: "--font-poppins",
-	subsets: ["latin"],
-	weight: ["400", "500", "600", "700"],
+  variable: "--font-poppins",
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
 });
 
 const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
-	subsets: ["latin"],
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
 const tangerine = Tangerine({
-	variable: "--font-tangerine",
-	subsets: ["latin"],
-	weight: "400",
-	style: "normal",
+  variable: "--font-tangerine",
+  subsets: ["latin"],
+  weight: "400",
+  style: "normal",
 });
 
 export const metadata: Metadata = {
-	title: {
-		template: "%s | LNHS Alumni Tracking",
-		default: "LNHS Alumni Tracking",
-	},
-	icons: { icon: "/icon.svg" },
+  title: {
+    template: "%s | BU Connect",
+    default: "BU Connect",
+  },
+  icons: { icon: "/icon.svg" },
 };
 
 export default async function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	const user = await getInformation();
-	const settings = await readSettingsAction();
+  const user = await getInformation();
+  const settings = await readSettingsAction();
 
-	return (
-		<html lang="en" suppressHydrationWarning>
-			<body
-				className={`${geistSans.variable} ${poppins.variable} ${tangerine.variable} ${geistMono.variable} antialiased `}>
-				<Providers>
-					{settings &&
-					settings.isMaintenance &&
-					user &&
-					user.role === "ALUMNI" ? (
-						<MaintenancePage />
-					) : (
-						<>
-							<Header user={user} />
-							{children}
-							{/* <Footer /> */}
-						</>
-					)}
-				</Providers>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${geistSans.variable} ${poppins.variable} ${tangerine.variable} ${geistMono.variable} antialiased `}
+      >
+        <Providers>
+          {settings &&
+          settings.isMaintenance &&
+          user &&
+          user.role === "ALUMNI" ? (
+            <MaintenancePage />
+          ) : (
+            <>
+              <Header user={user} />
+              {children}
+              {/* <Footer /> */}
+            </>
+          )}
+        </Providers>
+      </body>
+    </html>
+  );
 }
