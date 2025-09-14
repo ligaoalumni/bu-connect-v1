7f36a9dd215ddbf42de50099ffe260a0bc7565c2
awynx <alwin.puche16@gmail.com>
2025-09-15 02:57:44 +0800

    Add alumni announcement creation page

---
+++ src/app/(alumni)/announcements/add/page.tsx
@@ -0,0 +1,9 @@
+import AnnouncementForm from "@/app/admin/announcements/__components/announcement-form";
+
+export default function AddAnnouncement() {
+  return (
+    <div className="mt-10 px-5 md:px-10">
+      <AnnouncementForm />;
+    </div>
+  );
+}
