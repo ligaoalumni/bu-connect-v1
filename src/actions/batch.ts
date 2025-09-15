b2f59d78887f3a1a274bf55a52d58ca3e0182839
awynx <alwin.puche16@gmail.com>
2025-09-15 20:38:09 +0800

    Change batch ordering to descending in readBatches

--- src/actions/batch.ts
+++ src/actions/batch.ts
@@ -1,76 +1,74 @@
 "use server";
 
 import {
-	createBatch,
-	readBatch,
-	readBatches,
-	uploadBatchImages,
+  createBatch,
+  readBatch,
+  readBatches,
+  uploadBatchImages,
 } from "@/repositories";
-import { Pagination } from "@/types";
+import { PaginationArgs } from "@/types";
 import { revalidatePath } from "next/cache";
 
-export const readBatchesAction = async ({
-	pagination,
-}: {
-	pagination?: Pagination;
-} = {}) => {
-	try {
-		return await readBatches({ pagination });
-	} catch {
-		throw new Error("Failed to fetch batches");
-	}
+export const readBatchesAction = async (
+  args: PaginationArgs<never, never> = {},
+) => {
+  try {
+    return await readBatches(args);
+  } catch {
+    throw new Error("Failed to fetch batches");
+  }
 };
 
 export const createBatchAction = async (batchNumber: number) => {
-	try {
-		const isBatchExists = await readBatch(batchNumber);
-		if (isBatchExists) {
-			throw new Error("Batch already exists");
-		}
+  try {
+    const isBatchExists = await readBatch(batchNumber);
+    if (isBatchExists) {
+      throw new Error("Batch already exists");
+    }
 
-		const batch = await createBatch(batchNumber);
+    const batch = await createBatch(batchNumber);
 
-		revalidatePath("/admin/batches-gallery");
+    revalidatePath("/admin/batches-gallery");
 
-		return batch;
-	} catch (error) {
-		console.log(error, "qq");
-		throw new Error(
-			error instanceof Error ? error.message : "Failed to create batch"
-		);
-	}
+    return batch;
+  } catch (error) {
+    console.log(error, "qq");
+    throw new Error(
+      error instanceof Error ? error.message : "Failed to create batch",
+    );
+  }
 };
 
 export const uploadBatchImagesAction = async (
-	batchNumber: number,
-	images: string[]
+  batchNumber: number,
+  images: string[],
 ) => {
-	try {
-		const isBatchExists = await readBatch(batchNumber);
-		if (!isBatchExists) {
-			throw new Error("Batch does not exist");
-		}
-		const batch = await uploadBatchImages({
-			batch: batchNumber,
-			images,
-		});
+  try {
+    const isBatchExists = await readBatch(batchNumber);
+    if (!isBatchExists) {
+      throw new Error("Batch does not exist");
+    }
+    const batch = await uploadBatchImages({
+      batch: batchNumber,
+      images,
+    });
 
-		return batch;
-	} catch (error) {
-		console.log(error, "qq");
-		throw new Error(
-			error instanceof Error ? error.message : "Failed to create batch"
-		);
-	}
+    return batch;
+  } catch (error) {
+    console.log(error, "qq");
+    throw new Error(
+      error instanceof Error ? error.message : "Failed to create batch",
+    );
+  }
 };
 
 export const readBatchAction = async (batchNumber: number) => {
-	try {
-		return await readBatch(batchNumber);
-	} catch (error) {
-		console.log(error, "qq");
-		throw new Error(
-			error instanceof Error ? error.message : "Failed to create batch"
-		);
-	}
+  try {
+    return await readBatch(batchNumber);
+  } catch (error) {
+    console.log(error, "qq");
+    throw new Error(
+      error instanceof Error ? error.message : "Failed to create batch",
+    );
+  }
 };
--- src/repositories/batch.ts
+++ src/repositories/batch.ts
@@ -3,83 +3,83 @@ import { prisma } from "@/lib";
 import { Batch, PaginationArgs, PaginationResult } from "@/types";
 
 export const readBatches = async ({
-	pagination,
-	orderBy,
-	order,
+  pagination,
+  orderBy,
+  order,
 }: PaginationArgs<never, never>): Promise<PaginationResult<Batch>> => {
-	const batches = await prisma.batch.findMany({
-		skip: pagination ? pagination.limit * pagination.page : undefined,
-		take: pagination ? pagination.limit : undefined,
-		orderBy: orderBy ? { [orderBy]: order || "asc" } : { batch: "asc" },
-	});
+  const batches = await prisma.batch.findMany({
+    skip: pagination ? pagination.limit * pagination.page : undefined,
+    take: pagination ? pagination.limit : undefined,
+    orderBy: orderBy ? { [orderBy]: order || "desc" } : { batch: "desc" },
+  });
 
-	const batchesWithStudentCounts = await Promise.all(
-		batches.map(async (batch) => {
-			const students = await prisma.user.count({
-				where: {
-					batch: batch.batch,
-				},
-			});
+  const batchesWithStudentCounts = await Promise.all(
+    batches.map(async (batch) => {
+      const students = await prisma.user.count({
+        where: {
+          batch: batch.batch,
+        },
+      });
 
-			return {
-				id: batch.id,
-				batch: batch.batch,
-				images: batch.images,
-				students,
-			};
-		})
-	);
+      return {
+        id: batch.id,
+        batch: batch.batch,
+        images: batch.images,
+        students,
+      };
+    }),
+  );
 
-	const count = await prisma.batch.count();
+  const count = await prisma.batch.count();
 
-	return {
-		count,
-		hasMore: batches.length === pagination?.limit,
-		data: batchesWithStudentCounts,
-	};
+  return {
+    count,
+    hasMore: batches.length === pagination?.limit,
+    data: batchesWithStudentCounts,
+  };
 };
 
 export const createBatch = async (batchNumber: number) => {
-	return await prisma.batch.create({
-		data: {
-			batch: batchNumber,
-		},
-	});
+  return await prisma.batch.create({
+    data: {
+      batch: batchNumber,
+    },
+  });
 };
 
 export const readBatch = async (batchNumber: number) => {
-	const batch = await prisma.batch.findFirst({
-		where: {
-			batch: batchNumber,
-		},
-	});
+  const batch = await prisma.batch.findFirst({
+    where: {
+      batch: batchNumber,
+    },
+  });
 
-	if (!batch) return null;
+  if (!batch) return null;
 
-	const students = await prisma.user.count({
-		where: {
-			batch: batchNumber,
-		},
-	});
+  const students = await prisma.user.count({
+    where: {
+      batch: batchNumber,
+    },
+  });
 
-	return { ...batch, students };
+  return { ...batch, students };
 };
 
 export const uploadBatchImages = async ({
-	batch,
-	images,
+  batch,
+  images,
 }: {
-	batch: number;
-	images: string[];
+  batch: number;
+  images: string[];
 }) => {
-	return await prisma.batch.update({
-		where: {
-			batch,
-		},
-		data: {
-			images: {
-				push: images,
-			},
-		},
-	});
+  return await prisma.batch.update({
+    where: {
+      batch,
+    },
+    data: {
+      images: {
+        push: images,
+      },
+    },
+  });
 };
