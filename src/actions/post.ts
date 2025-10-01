cc06eb4a2482d0bc4c33a3e6e9d5d1dfacbb67c8
awynx <alwin.puche16@gmail.com>
2025-10-01 15:10:40 +0800

    Revalidate path after creating a post and liking actions
    Add revalidatePath("/") after creating a post and revalidatePath for
    like/unlike/comment actions to ensure

--- src/actions/post.ts
+++ src/actions/post.ts
@@ -2,155 +2,156 @@
 
 import { decrypt } from "@/lib/session";
 import {
-	createPost,
-	likePost,
-	readPost,
-	readPostComments,
-	readPosts,
-	unlikePost,
-	updatePost,
-	writePostComment,
+  createPost,
+  likePost,
+  readPost,
+  readPostComments,
+  readPosts,
+  unlikePost,
+  updatePost,
+  writePostComment,
 } from "@/repositories";
 import { CreatePost, Pagination, PaginationArgs } from "@/types";
 import { revalidatePath } from "next/cache";
 import { cookies } from "next/headers";
 
 export const createPostAction = async (
-	data: Omit<CreatePost, "postedById">
+  data: Omit<CreatePost, "postedById">,
 ) => {
-	try {
-		const cookieStore = await cookies();
+  try {
+    const cookieStore = await cookies();
 
-		const session = await decrypt(cookieStore.get("session")?.value);
+    const session = await decrypt(cookieStore.get("session")?.value);
 
-		await createPost({
-			...data,
-			postedById: Number(session?.id),
-		});
-	} catch (error) {
-		console.log(error);
-		throw new Error("Failed to create event");
-	}
+    await createPost({
+      ...data,
+      postedById: Number(session?.id),
+    });
+    revalidatePath("/");
+  } catch (error) {
+    console.log(error);
+    throw new Error("Failed to create event");
+  }
 };
 
 export const updatePostAction = async (
-	slug: string,
-	data: Partial<Omit<CreatePost, "postedById">>
+  slug: string,
+  data: Partial<Omit<CreatePost, "postedById">>,
 ) => {
-	try {
-		const cookieStore = await cookies();
+  try {
+    const cookieStore = await cookies();
 
-		const session = await decrypt(cookieStore.get("session")?.value);
+    const session = await decrypt(cookieStore.get("session")?.value);
 
-		await updatePost({
-			slug,
-			postedById: Number(session?.id),
-			data,
-		});
-	} catch (error) {
-		console.log(error);
-		throw new Error("Failed to create event");
-	}
+    await updatePost({
+      slug,
+      postedById: Number(session?.id),
+      data,
+    });
+  } catch (error) {
+    console.log(error);
+    throw new Error("Failed to create event");
+  }
 };
 
 export const readPostsAction = async (
-	data: PaginationArgs<never, never> = {}
+  data: PaginationArgs<never, never> = {},
 ) => {
-	try {
-		return await readPosts(data);
-	} catch (error) {
-		console.log(error);
-		throw new Error("Failed to fetch posts");
-	}
+  try {
+    return await readPosts(data);
+  } catch (error) {
+    console.log(error);
+    throw new Error("Failed to fetch posts");
+  }
 };
 
 export const readPostAction = async (slug: string) => {
-	try {
-		return await readPost(slug);
-	} catch (error) {
-		console.log(error);
-		throw new Error("Failed to fetch posts");
-	}
+  try {
+    return await readPost(slug);
+  } catch (error) {
+    console.log(error);
+    throw new Error("Failed to fetch posts");
+  }
 };
 
 export const readPostCommentsAction = async (args: {
-	pagination?: Pagination;
-	postId: number;
+  pagination?: Pagination;
+  postId: number;
 }) => {
-	try {
-		return await readPostComments(args);
-	} catch (err) {
-		console.log(err);
-		throw new Error("Failed to fetch comments");
-	}
+  try {
+    return await readPostComments(args);
+  } catch (err) {
+    console.log(err);
+    throw new Error("Failed to fetch comments");
+  }
 };
 
 export const writePostCommentAction = async ({
-	comment,
-	postId,
-	slug,
+  comment,
+  postId,
+  slug,
 }: {
-	postId: number;
-	comment: string;
-	slug: string;
+  postId: number;
+  comment: string;
+  slug: string;
 }) => {
-	try {
-		const cookieStore = await cookies();
+  try {
+    const cookieStore = await cookies();
 
-		const session = await decrypt(cookieStore.get("session")?.value);
+    const session = await decrypt(cookieStore.get("session")?.value);
 
-		if (!session?.id) throw new Error("Unauthorized");
+    if (!session?.id) throw new Error("Unauthorized");
 
-		await writePostComment({ userId: session.id, comment, postId });
+    await writePostComment({ userId: session.id, comment, postId });
 
-		revalidatePath(`/posts/${slug}/info`);
-	} catch {
-		throw new Error("Failed to fetch interested alumni");
-	}
+    revalidatePath(`/posts/${slug}/info`);
+  } catch {
+    throw new Error("Failed to fetch interested alumni");
+  }
 };
 
 export const unlikePostAction = async ({
-	postId,
-	slug,
+  postId,
+  slug,
 }: {
-	postId: number;
+  postId: number;
 
-	slug: string;
+  slug: string;
 }) => {
-	try {
-		const cookieStore = await cookies();
+  try {
+    const cookieStore = await cookies();
 
-		const session = await decrypt(cookieStore.get("session")?.value);
+    const session = await decrypt(cookieStore.get("session")?.value);
 
-		if (!session?.id) throw new Error("Unauthorized");
+    if (!session?.id) throw new Error("Unauthorized");
 
-		await unlikePost({ userId: session.id, postId });
+    await unlikePost({ userId: session.id, postId });
 
-		revalidatePath(`/posts/${slug}/info`);
-	} catch {
-		throw new Error("Failed to fetch interested alumni");
-	}
+    revalidatePath(`/posts/${slug}/info`);
+  } catch {
+    throw new Error("Failed to fetch interested alumni");
+  }
 };
 
 export const likePostAction = async ({
-	postId,
-	slug,
+  postId,
+  slug,
 }: {
-	postId: number;
+  postId: number;
 
-	slug: string;
+  slug: string;
 }) => {
-	try {
-		const cookieStore = await cookies();
+  try {
+    const cookieStore = await cookies();
 
-		const session = await decrypt(cookieStore.get("session")?.value);
+    const session = await decrypt(cookieStore.get("session")?.value);
 
-		if (!session?.id) throw new Error("Unauthorized");
+    if (!session?.id) throw new Error("Unauthorized");
 
-		await likePost({ userId: session.id, postId });
+    await likePost({ userId: session.id, postId });
 
-		revalidatePath(`/posts/${slug}/info`);
-	} catch {
-		throw new Error("Failed to fetch interested alumni");
-	}
+    revalidatePath(`/posts/${slug}/info`);
+  } catch {
+    throw new Error("Failed to fetch interested alumni");
+  }
 };
--- src/actions/user.ts
+++ src/actions/user.ts
@@ -4,76 +4,78 @@ import { generateEmailHTML } from "@/lib";
 import { decrypt, encrypt } from "@/lib/session";
 import { transporter } from "@/lib/email";
 import {
-  changeEmail,
-  connectAccount,
-  createUser,
-  getOldAccountsToVerify,
-  readUser,
-  readUsers,
-  readUsersUpdatedInLastDays,
-  updateLocationSharing,
-  updatePasswordWithCurrentPassword,
-  updateProfile,
-  updateUser,
-  updateUserStatus,
-  validateToken,
+	changeEmail,
+	connectAccount,
+	createUser,
+	getOldAccountsToVerify,
+	readUser,
+	readUsers,
+	readUsersUpdatedInLastDays,
+	updateLocationSharing,
+	updatePasswordWithCurrentPassword,
+	updateProfile,
+	updateUser,
+	updateUserStatus,
+	validateToken,
 } from "@/repositories";
 import type {
-  AdminFormData,
-  PaginationArgs,
-  PaginationResult,
-  UpdateProfileData,
-  UpdateUserArgs,
-  UserRole,
+	AdminFormData,
+	PaginationArgs,
+	PaginationResult,
+	UpdateProfileData,
+	UpdateUserArgs,
+	UserRole,
 } from "@/types";
 import { User } from "@prisma/client";
 import bcrypt from "bcryptjs";
 import { revalidatePath } from "next/cache";
 import { cookies } from "next/headers";
 
+export const functionName = async (/* arguments/parameters */) => {};
+
 export const createAdmin = async (
-  data: Pick<
-    AdminFormData,
-    "firstName" | "lastName" | "middleName" | "password" | "email" | "role"
-  >,
+	data: Pick<
+		AdminFormData,
+		"firstName" | "lastName" | "middleName" | "password" | "email" | "role"
+	>
 ) => {
-  try {
-    // Your code here
-    const isExists = await readUser(-1, data.email);
+	try {
+		// Your code here
+		const isExists = await readUser(-1, data.email);
 
-    if (isExists) {
-      throw new Error("An admin with the same email address already exists.");
-    }
+		if (isExists) {
+			throw new Error("An admin with the same email address already exists.");
+		}
 
-    // e.g. Hash the user's password before storing it
-    const hashedPassword = await bcrypt.hash(data.password, 10);
+		// e.g. Hash the user's password before storing it
+		const hashedPassword = await bcrypt.hash(data.password, 10);
 
-    const newAdmin = await createUser({
-      avatar: null,
-      middleName: data.middleName || "",
-      password: hashedPassword,
-      birthDate: new Date(),
-      batchYear: -1,
-      program: "",
-      email: data.email,
-      firstName: data.firstName,
-      lastName: data.lastName,
-      role: data.role as UserRole,
-    });
+		const newAdmin = await createUser({
+			avatar: null,
+			middleName: data.middleName || "",
+			password: hashedPassword,
+			birthDate: new Date(),
+			batchYear: -1,
+			program: "",
+			email: data.email,
+			firstName: data.firstName,
+			lastName: data.lastName,
+			role: data.role as UserRole,
+		});
 
-    if (!newAdmin) {
-      throw new Error("An error occurred while creating the admin.");
-    }
+		if (!newAdmin) {
+			throw new Error("An error occurred while creating the admin.");
+		}
 
-    const mailOptions = {
-      from: process.env.EMAIL,
-      sender: {
-        name: "BU Connect",
-        address: process.env.EMAIL!,
-      },
-      to: data.email,
-      subject: "Account Credentials",
-      html: generateEmailHTML(`
+		const mailOptions = {
+			from: process.env.EMAIL,
+			sender: {
+				name: "BU Connect",
+				address: process.env.EMAIL!,
+			},
+			to: data.email,
+			subject: "Account Credentials",
+			html: generateEmailHTML(`
                 <p>Hello ${data.firstName} ${data.lastName},</p>
 
                 <p>Your administrator account has been created. Below are your login credentials:</p>
@@ -95,252 +97,252 @@ export const createAdmin = async (
 
                 <p>Thank you,<br>The Team</p>
             `),
-    };
+		};
 
-    await transporter.sendMail(mailOptions);
-  } catch (err) {
-    throw new Error(
-      err instanceof Error
-        ? err.message
-        : "An error occurred while creating the admin.",
-    );
-  }
+		await transporter.sendMail(mailOptions);
+	} catch (err) {
+		throw new Error(
+			err instanceof Error
+				? err.message
+				: "An error occurred while creating the admin."
+		);
+	}
 };
 
 export const updateUserStatusAction = async (
-  id: number,
-  status: User["status"],
+	id: number,
+	status: User["status"]
 ) => {
-  await updateUserStatus(id, status);
-  revalidatePath("/admin/list");
+	await updateUserStatus(id, status);
+	revalidatePath("/admin/list");
 };
 
 export const verifyAccount = async (
-  userId: number,
-  email: string,
-  token: string,
+	userId: number,
+	email: string,
+	token: string
 ) => {
-  try {
-    const isValid = await validateToken(email, token);
+	try {
+		const isValid = await validateToken(email, token);
 
-    if (!isValid) {
-      throw new Error("OTP is expired or invalid!");
-    }
+		if (!isValid) {
+			throw new Error("OTP is expired or invalid!");
+		}
 
-    const user = await updateUser(userId, { verifiedAt: new Date() });
+		const user = await updateUser(userId, { verifiedAt: new Date() });
 
-    const { email: userEmail, id, role } = user;
-    const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
-    const session = await encrypt({
-      id,
-      role,
-      email: userEmail,
-      expiresAt,
-      verified: !!user.verifiedAt,
-    });
+		const { email: userEmail, id, role } = user;
+		const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
+		const session = await encrypt({
+			id,
+			role,
+			email: userEmail,
+			expiresAt,
+			verified: !!user.verifiedAt,
+		});
 
-    const cookieStore = await cookies();
+		const cookieStore = await cookies();
 
-    cookieStore.set("session", session, {
-      httpOnly: true,
-      secure: true,
-      expires: expiresAt,
-      sameSite: "lax",
-      path: "/",
-    });
-  } catch (err) {
-    throw new Error(
-      err instanceof Error
-        ? err.message
-        : "An error occurred while creating the admin.",
-    );
-  }
+		cookieStore.set("session", session, {
+			httpOnly: true,
+			secure: true,
+			expires: expiresAt,
+			sameSite: "lax",
+			path: "/",
+		});
+	} catch (err) {
+		throw new Error(
+			err instanceof Error
+				? err.message
+				: "An error occurred while creating the admin."
+		);
+	}
 };
 
 export const updateProfileActions = async (
-  id: number,
-  data: UpdateProfileData,
-  alumni?: boolean,
+	id: number,
+	data: UpdateProfileData,
+	alumni?: boolean
 ) => {
-  try {
-    await updateProfile(id, data);
+	try {
+		await updateProfile(id, data);
 
-    revalidatePath(alumni ? "/profile" : "/admin/profile");
-  } catch (err) {
-    throw new Error(
-      err instanceof Error
-        ? err.message
-        : "An error occurred while creating the admin.",
-    );
-  }
+		revalidatePath(alumni ? "/profile" : "/admin/profile");
+	} catch (err) {
+		throw new Error(
+			err instanceof Error
+				? err.message
+				: "An error occurred while creating the admin."
+		);
+	}
 };
 
 export const updateProfileStatusAction = async (
-  id: number,
-  data: UpdateProfileData,
+	id: number,
+	data: UpdateProfileData
 ) => {
-  try {
-    await updateProfile(id, data);
-  } catch (err) {
-    throw new Error(
-      err instanceof Error
-        ? err.message
-        : "An error occurred while updating your profile.",
-    );
-  }
+	try {
+		await updateProfile(id, data);
+	} catch (err) {
+		throw new Error(
+			err instanceof Error
+				? err.message
+				: "An error occurred while updating your profile."
+		);
+	}
 };
 
 export const updatePasswordAction = async ({
-  currentPassword,
-  id,
-  newPassword,
+	currentPassword,
+	id,
+	newPassword,
 }: {
-  id: number;
-  currentPassword: string;
-  newPassword: string;
+	id: number;
+	currentPassword: string;
+	newPassword: string;
 }) => {
-  try {
-    await updatePasswordWithCurrentPassword({
-      currentPassword,
-      id,
-      newPassword,
-    });
-  } catch (err) {
-    throw new Error(
-      err instanceof Error
-        ? err.message
-        : "An error occurred while updating your password.",
-    );
-  }
+	try {
+		await updatePasswordWithCurrentPassword({
+			currentPassword,
+			id,
+			newPassword,
+		});
+	} catch (err) {
+		throw new Error(
+			err instanceof Error
+				? err.message
+				: "An error occurred while updating your password."
+		);
+	}
 };
 
 export const changeEmailAction = async ({ email }: { email: string }) => {
-  try {
-    await changeEmail({ email });
-  } catch (err) {
-    throw new Error(
-      err instanceof Error
-        ? err.message
-        : "An error occurred while sending OTP to your new email address.",
-    );
-  }
+	try {
+		await changeEmail({ email });
+	} catch (err) {
+		throw new Error(
+			err instanceof Error
+				? err.message
+				: "An error occurred while sending OTP to your new email address."
+		);
+	}
 };
 
 export const readUsersAction = async ({
-  filter,
-  order,
-  orderBy,
-  pagination,
-  role = ["ADMIN", "ALUMNI"],
-  batch,
+	filter,
+	order,
+	orderBy,
+	pagination,
+	role = ["ADMIN", "ALUMNI"],
+	batch,
 }: PaginationArgs<never, UserRole> & { batch?: number } = {}): Promise<
-  PaginationResult<Omit<User, "password" | "notifications" | "rate">>
+	PaginationResult<Omit<User, "password" | "notifications" | "rate">>
 > => {
-  try {
-    return await readUsers({
-      filter,
-      order,
-      orderBy,
-      pagination,
-      role,
-      batch,
-    });
-  } catch (err) {
-    throw new Error(
-      err instanceof Error
-        ? err.message
-        : "An error occurred while sending OTP to your new email address.",
-    );
-  }
+	try {
+		return await readUsers({
+			filter,
+			order,
+			orderBy,
+			pagination,
+			role,
+			batch,
+		});
+	} catch (err) {
+		throw new Error(
+			err instanceof Error
+				? err.message
+				: "An error occurred while sending OTP to your new email address."
+		);
+	}
 };
 
 export const readUserAction = async (studentId: number) => {
-  try {
-    return await readUser(studentId);
-  } catch (err) {
-    throw new Error(
-      err instanceof Error
-        ? err.message
-        : "An error occurred while sending OTP to your new email address.",
-    );
-  }
+	try {
+		return await readUser(studentId);
+	} catch (err) {
+		throw new Error(
+			err instanceof Error
+				? err.message
+				: "An error occurred while sending OTP to your new email address."
+		);
+	}
 };
 
 export const updateLocationSharingAction = async (shareLocation: boolean) => {
-  try {
-    const cookieStore = await cookies();
+	try {
+		const cookieStore = await cookies();
 
-    const session = await decrypt(cookieStore.get("session")?.value);
+		const session = await decrypt(cookieStore.get("session")?.value);
 
-    if (session == null) throw new Error("Session expired!");
+		if (session == null) throw new Error("Session expired!");
 
-    await updateLocationSharing(session.id, shareLocation);
+		await updateLocationSharing(session.id, shareLocation);
 
-    revalidatePath(session.role == "ALUMNI" ? "/profile" : "/admin/profile");
-  } catch (err) {
-    throw new Error(
-      err instanceof Error
-        ? err.message
-        : "An error occurred while sending OTP to your new email address.",
-    );
-  }
+		revalidatePath(session.role == "ALUMNI" ? "/profile" : "/admin/profile");
+	} catch (err) {
+		throw new Error(
+			err instanceof Error
+				? err.message
+				: "An error occurred while sending OTP to your new email address."
+		);
+	}
 };
 
 export const updateUserAction = async (data: UpdateUserArgs) => {
-  try {
-    const cookieStore = await cookies();
+	try {
+		const cookieStore = await cookies();
 
-    const session = await decrypt(cookieStore.get("session")?.value);
+		const session = await decrypt(cookieStore.get("session")?.value);
 
-    if (session == null) throw new Error("Session expired!");
+		if (session == null) throw new Error("Session expired!");
 
-    await updateUser(session.id, data);
-  } catch (err) {
-    throw new Error(
-      err instanceof Error
-        ? err.message
-        : "An error occurred while updating the user.",
-    );
-  }
+		await updateUser(session.id, data);
+	} catch (err) {
+		throw new Error(
+			err instanceof Error
+				? err.message
+				: "An error occurred while updating the user."
+		);
+	}
 };
 
 export const getOldAccountsToVerifyAction = async (
-  args: PaginationArgs<never, UserRole>,
+	args: PaginationArgs<never, UserRole>
 ) => {
-  try {
-    return await getOldAccountsToVerify(args);
-  } catch (err) {
-    throw new Error(
-      err instanceof Error
-        ? err.message
-        : "An error occurred while sending OTP to your new email address.",
-    );
-  }
+	try {
+		return await getOldAccountsToVerify(args);
+	} catch (err) {
+		throw new Error(
+			err instanceof Error
+				? err.message
+				: "An error occurred while sending OTP to your new email address."
+		);
+	}
 };
 
 export const connectAccountAction = async (data: {
-  id: number;
-  oldAccountId: number;
+	id: number;
+	oldAccountId: number;
 }) => {
-  try {
-    return await connectAccount(data);
-  } catch (err) {
-    throw new Error(
-      err instanceof Error
-        ? err.message
-        : "An error occurred while sending OTP to your new email address.",
-    );
-  }
+	try {
+		return await connectAccount(data);
+	} catch (err) {
+		throw new Error(
+			err instanceof Error
+				? err.message
+				: "An error occurred while sending OTP to your new email address."
+		);
+	}
 };
 
 export const readUsersUpdatedInLastDaysAction = async (days?: number) => {
-  try {
-    return await readUsersUpdatedInLastDays(days);
-  } catch (err) {
-    throw new Error(
-      err instanceof Error
-        ? err.message
-        : "An error occurred while sending OTP to your new email address.",
-    );
-  }
+	try {
+		return await readUsersUpdatedInLastDays(days);
+	} catch (err) {
+		throw new Error(
+			err instanceof Error
+				? err.message
+				: "An error occurred while sending OTP to your new email address."
+		);
+	}
 };
