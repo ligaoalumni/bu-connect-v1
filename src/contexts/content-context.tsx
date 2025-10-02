85b9001b49e8a073621d0faf8947595279cbfe28
awynx <alwin.puche16@gmail.com>
2025-10-02 17:01:40 +0800

    Add posts state to ContentContext and provider

--- src/contexts/content-context.tsx
+++ src/contexts/content-context.tsx
@@ -1,41 +1,47 @@
 "use client";
 
+import { TPost } from "@/types";
 import {
-	createContext,
-	useState,
-	useContext,
-	ReactNode,
-	SetStateAction,
-	Dispatch,
+  createContext,
+  useState,
+  useContext,
+  ReactNode,
+  SetStateAction,
+  Dispatch,
 } from "react";
 
 interface ContentContextType {
-	data: {
-		id: number;
-		likes: number;
-		comments: number;
-		isLiked?: boolean;
-	} | null;
-	setData: Dispatch<SetStateAction<ContentContextType["data"] | null>>;
+  data: {
+    id: number;
+    likes: number;
+    comments: number;
+    isLiked?: boolean;
+  } | null;
+  setData: Dispatch<SetStateAction<ContentContextType["data"] | null>>;
+  posts: TPost[];
+  setPosts: Dispatch<SetStateAction<TPost[]>>;
 }
 
 const ContentContext = createContext<ContentContextType>({
-	data: null,
-	setData: () => null,
+  data: null,
+  setData: () => null,
+  posts: [],
+  setPosts: () => null,
 });
 
 interface ContentProviderProps {
-	children: ReactNode;
+  children: ReactNode;
 }
 
 export const ContentProvider = ({ children }: ContentProviderProps) => {
-	const [data, setData] = useState<null | ContentContextType["data"]>(null);
+  const [data, setData] = useState<null | ContentContextType["data"]>(null);
+  const [posts, setPosts] = useState<TPost[]>([]);
 
-	return (
-		<ContentContext.Provider value={{ data, setData }}>
-			{children}
-		</ContentContext.Provider>
-	);
+  return (
+    <ContentContext.Provider value={{ data, setData, posts, setPosts }}>
+      {children}
+    </ContentContext.Provider>
+  );
 };
 
 export const useContentData = () => useContext(ContentContext);
