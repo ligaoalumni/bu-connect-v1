cd25b32ee09cd6ee894b316b4ab8bb8e04a2a602
awynx <alwin.puche16@gmail.com>
2025-10-02 21:37:27 +0800

    Update posts page to use readPostsAction and improve pagination

--- src/app/(alumni)/posts/page.tsx
+++ src/app/(alumni)/posts/page.tsx
@@ -1,14 +1,18 @@
-import { readMyPostsAction } from "@/actions";
+import { readPostsAction } from "@/actions";
 import { PostsInfiniteScroll } from "@/app/(alumni)/posts/__components/posts-infinite-scroll";
 import React from "react";
 
 export default async function Page() {
-  const posts = await readMyPostsAction({
+  const posts = await readPostsAction({
     pagination: {
       page: 0,
-      limit: 10,
+      limit: 8,
     },
+    order: "desc",
+    orderBy: "createdAt",
   });
 
-  return <PostsInfiniteScroll defaultData={posts.data} />;
+  return (
+    <PostsInfiniteScroll defaultData={posts.data} moreData={posts.hasMore} />
+  );
 }
