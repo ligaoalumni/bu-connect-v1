890b478fdeb564aefb349c35f73e5cdc1db720fc
awynx <alwin.puche16@gmail.com>
2025-10-03 23:04:55 +0800

    update prop id type

--- src/app/admin/_components/delete-modal.tsx
+++ src/app/admin/_components/delete-modal.tsx
@@ -11,7 +11,7 @@ import {
 } from "@/components/ui/dialog";
 
 interface DeleteModalProps {
-  id: number;
+  id: number | string;
   onDelete: VoidFunction;
   onClose: VoidFunction;
   title: string;
